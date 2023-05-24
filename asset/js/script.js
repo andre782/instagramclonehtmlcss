333b134b7cb1ca117f74ba17c5c4155dd544fbf0
Andre setyo <andreaswinarno1212@gmail.com>
2023-05-24 13:30:23 +0700

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,9 @@
+function menuLIke(){
+    var element = document.getElementById("menu-like");
+    element.classList.toggle("menuLike");
+  }
+  
+  function menuprofile(){
+      var element = document.getElementById("menu-profile");
+      element.classList.toggle("Menuprofile");
+    } 
